591b60f55edeffbbe5db2650b3858e9929ea32d5
milescajus <80205080+milescajus@users.noreply.github.com>
2021-12-13 01:45:43 +0100

    added basic testing in `main()` but more thorough testing occurs in `test.cpp`

--- main.cpp
+++ main.cpp
@@ -1,1 +1,18 @@
-int main() {}
+#include "logic.hpp"
+
+int main()
+{
+    auto barista = CoffeeMaker();
+    barista.order(-1);
+    barista.order(1);
+    auto my_coffee = barista.collect();
+
+    std::cout << "I received " << my_coffee.size() << " cups of coffee." << std::endl;
+
+    barista.order(5);
+    barista.order(2);
+    barista.order(3);
+    auto my_other_coffee = barista.collect();
+
+    std::cout << "I received " << my_other_coffee.size() << " cups of coffee." << std::endl;
+}
